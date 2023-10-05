17e464614762374627b52dd301671d3707754f12
umardar8 <umerfarooqdar.official@gmail.com>
2023-10-05 13:49:55 +0500

    Initial commit

---
+++ app/src/main/java/pk/internee/MainActivity.kt
@@ -0,0 +1,61 @@
+package pk.internee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment(), it.title.toString())
+                R.id.about -> replaceFragment(AboutFragment(), it.title.toString())
+                R.id.contact -> replaceFragment(ContactFragment(), it.title.toString())
+                R.id.lms -> replaceFragment(LMSFragment(), it.title.toString())
+                R.id.interneePortal -> replaceFragment(InterneeFragment(), it.title.toString())
+                R.id.internships -> replaceFragment(InternshipsFragment(), it.title.toString())
+                R.id.jobPortal -> replaceFragment(JobFragment(), it.title.toString())
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, title: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+        setTitle(title)
+        drawerLayout.close()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
